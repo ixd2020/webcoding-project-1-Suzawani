5087918cb80156e2d04a91c3aaec4d947ed62473
Glitch (hayao-miyazaki) <none>
2020-12-17 06:06:05 +0000

    📷📅 Updated with Glitch

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.art .letters, .awards .letters, .films .letters, .header .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: ('.art .letter, .awards .letter, .films .letter, .header .letter'),
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 750,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i
+  });
+
+$(document).ready(function(){
+  $("#oscar").click(function(){
+    $("#panel1").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#japan").click(function(){
+    $("#panel2").slideToggle("slow");
+  });
+});
